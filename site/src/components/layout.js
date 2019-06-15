c35216587e60ff8fe5160122d9958ac94aa0d93f
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-15 15:04:55 +0800

    trying to do styling

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -7,12 +7,24 @@
 
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import Nav from "./Nav";
+//running a graphQL query inside of Layout component
 
 export default function Layout({ children }) {
+    const data = useStaticQuery(graphql`
+  query SiteData {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`)
   return (
     <div>
-      <h1>My Website!</h1>
-      {children}
+        <Nav />
+        <h1>{data.site.siteMetadata.title}</h1>
+        {children}
     </div>
   )
 }
